38ab63438a8aadc286a391d06f40d156c1cf1f6c
lfeq <panchocosa@gmail.com>
2024-01-22 11:09:43 -0600

    Added instant toggle

--- Cellular Automata/Assets/Scripts/GridGenerator.cs
+++ Cellular Automata/Assets/Scripts/GridGenerator.cs
@@ -17,12 +17,13 @@ public class GridGenerator : MonoBehaviour {
     [SerializeField] private TMP_InputField widthInput;
     [SerializeField] private TMP_InputField ruleInput;
     [SerializeField] private Toggle randomCenterToggle;
+    [SerializeField] private Toggle instantToggle;
 
-    private int gridWidth = 250;
-    private int gridHeight = 250;
-    private int rule = 250;
-    private int[] combination = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
-    private List<GameObject> allTiles = new List<GameObject>();
+    private int m_gridWidth = 250;
+    private int m_gridHeight = 250;
+    private int m_rule = 250;
+    private int[] m_combination = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+    private List<GameObject> m_allTiles = new List<GameObject>();
 
     /// <summary>
     /// Generates the grid of tiles based on the specified parameters and cellular automaton rule.
@@ -30,38 +31,42 @@ public class GridGenerator : MonoBehaviour {
     public void GenerateCells() {
         ClearLevel();
         SetUpRule();
-        StartCoroutine(SpawnDelay());
+        if (instantToggle.isOn) {
+            SpawnWithoutDelay();
+        } else {
+            StartCoroutine(SpawnDelay());
+        }
     }
 
     /// <summary>
     /// Sets the grid height based on the input field value.
     /// </summary>
     public void SetHeight() {
-        gridHeight = int.Parse(heightInput.text);
+        m_gridHeight = int.Parse(heightInput.text);
     }
 
     /// <summary>
     /// Sets the grid width based on the input field value.
     /// </summary>
     public void SetWidth() {
-        gridWidth = int.Parse(widthInput.text);
+        m_gridWidth = int.Parse(widthInput.text);
     }
 
     /// <summary>
     /// Sets the cellular automaton rule based on the input field value.
     /// </summary>
     public void SetRule() {
-        rule = int.Parse(ruleInput.text);
+        m_rule = int.Parse(ruleInput.text);
     }
 
     /// <summary>
     /// Clears all existing tiles in the grid.
     /// </summary>
     private void ClearLevel() {
-        for (int i = 0; i < allTiles.Count; i++) {
-            Destroy(allTiles[i]);
+        for (int i = 0; i < m_allTiles.Count; i++) {
+            Destroy(m_allTiles[i]);
         }
-        allTiles.Clear();
+        m_allTiles.Clear();
     }
 
     /// <summary>
@@ -72,16 +77,16 @@ public class GridGenerator : MonoBehaviour {
     // Convert char to int code got from this link:
     // https://stackoverflow.com/questions/239103/convert-char-to-int-in-c-sharp
     private void SetUpRule() {
-        if (rule > 255) {
+        if (m_rule > 255) {
             throw new Exception("Rule number must be smaller than 256.");
         }
-        string binary = Convert.ToString(rule, 2);
+        string binary = Convert.ToString(m_rule, 2);
         int j = 1;
-        for (int i = combination.Length - 1; i >= 0 - 1; i--, j++) {
+        for (int i = m_combination.Length - 1; i >= 0 - 1; i--, j++) {
             if (j > binary.Length) {
                 break;
             }
-            combination[i] = binary[binary.Length - j] - '0';
+            m_combination[i] = binary[binary.Length - j] - '0';
         }
     }
 
@@ -89,16 +94,16 @@ public class GridGenerator : MonoBehaviour {
     /// Spawns tiles in the grid with a specified delay between each tile.
     /// </summary>
     private IEnumerator SpawnDelay() {
-        int center = gridWidth / 2;
+        int center = m_gridWidth / 2;
         if (randomCenterToggle.isOn) {
-            center = Random.Range(0, gridWidth);
+            center = Random.Range(0, m_gridWidth);
         }
-        for (int y = 0; y < gridHeight; y++) {
-            for (int x = 0; x < gridWidth; x++) {
+        for (int y = 0; y < m_gridHeight; y++) {
+            for (int x = 0; x < m_gridWidth; x++) {
                 yield return new WaitForSeconds(speed);
                 Vector3 spawnPos = new Vector3(x, -y, 0);
                 GameObject tempTile = Instantiate(tilePrefab, spawnPos, Quaternion.identity);
-                allTiles.Add(tempTile);
+                m_allTiles.Add(tempTile);
                 tempTile.name = x.ToString() + " -" + y.ToString();
                 tempTile.tag = "0"; //Not painted
                 // Skip the first row
@@ -115,7 +120,55 @@ public class GridGenerator : MonoBehaviour {
                 upperTilePosY = Mathf.Abs(upperTilePosY);
                 GameObject upperTileGameObject = GameObject.Find(x.ToString() + " -" + upperTilePosY.ToString());
                 upperTiles.y = int.Parse(upperTileGameObject.tag.ToString());
-                if (x == gridWidth - 1) {
+                if (x == m_gridWidth - 1) {
+                    upperTiles.z = 0;
+                } else {
+                    int upperTileLeft = x + 1; //Actually right
+                    GameObject upperLeftTileGameObject = GameObject.Find(upperTileLeft.ToString() + " -" + upperTilePosY.ToString());
+                    upperTiles.z = int.Parse(upperLeftTileGameObject.tag.ToString());
+                }
+                if (x == 0) {
+                    upperTiles.x = 0;
+                } else {
+                    int upperTileRight = x - 1; // Actually left
+                    GameObject upperRightTileGameObject = GameObject.Find(upperTileRight.ToString() + " -" + upperTilePosY.ToString());
+                    upperTiles.x = int.Parse(upperRightTileGameObject.tag.ToString());
+                }
+                if (PaintTile(upperTiles)) {
+                    tempTile.GetComponent<SpriteRenderer>().material.color = paintedTileColor;
+                    tempTile.tag = "1"; // Painted
+                }
+            }
+        }
+    }
+
+    private void SpawnWithoutDelay() {
+        int center = m_gridWidth / 2;
+        if (randomCenterToggle.isOn) {
+            center = Random.Range(0, m_gridWidth);
+        }
+        for (int y = 0; y < m_gridHeight; y++) {
+            for (int x = 0; x < m_gridWidth; x++) {
+                Vector3 spawnPos = new Vector3(x, -y, 0);
+                GameObject tempTile = Instantiate(tilePrefab, spawnPos, Quaternion.identity);
+                m_allTiles.Add(tempTile);
+                tempTile.name = x.ToString() + " -" + y.ToString();
+                tempTile.tag = "0"; //Not painted
+                // Skip the first row
+                if (y == 0) {
+                    if (x == center) {
+                        tempTile.GetComponent<SpriteRenderer>().material.color = paintedTileColor;
+                        tempTile.tag = "1"; // Painted
+                    }
+                    continue;
+                }
+                // Start painting tiles
+                Vector3 upperTiles = Vector3.zero;
+                int upperTilePosY = -y + 1;
+                upperTilePosY = Mathf.Abs(upperTilePosY);
+                GameObject upperTileGameObject = GameObject.Find(x.ToString() + " -" + upperTilePosY.ToString());
+                upperTiles.y = int.Parse(upperTileGameObject.tag.ToString());
+                if (x == m_gridWidth - 1) {
                     upperTiles.z = 0;
                 } else {
                     int upperTileLeft = x + 1; //Actually right
@@ -147,21 +200,21 @@ public class GridGenerator : MonoBehaviour {
     /// </returns>
     private bool PaintTile(Vector3 upperTreeTiles) {
         if ((upperTreeTiles.x == 1) && (upperTreeTiles.y == 1) && (upperTreeTiles.z == 1)) {
-            return Convert.ToBoolean(combination[0]);
+            return Convert.ToBoolean(m_combination[0]);
         } else if ((upperTreeTiles.x == 1) && (upperTreeTiles.y == 1) && (upperTreeTiles.z == 0)) {
-            return Convert.ToBoolean(combination[1]);
+            return Convert.ToBoolean(m_combination[1]);
         } else if ((upperTreeTiles.x == 1) && (upperTreeTiles.y == 0) && (upperTreeTiles.z == 1)) {
-            return Convert.ToBoolean(combination[2]);
+            return Convert.ToBoolean(m_combination[2]);
         } else if ((upperTreeTiles.x == 1) && (upperTreeTiles.y == 0) && (upperTreeTiles.z == 0)) {
-            return Convert.ToBoolean(combination[3]);
+            return Convert.ToBoolean(m_combination[3]);
         } else if ((upperTreeTiles.x == 0) && (upperTreeTiles.y == 1) && (upperTreeTiles.z == 1)) {
-            return Convert.ToBoolean(combination[4]);
+            return Convert.ToBoolean(m_combination[4]);
         } else if ((upperTreeTiles.x == 0) && (upperTreeTiles.y == 1) && (upperTreeTiles.z == 0)) {
-            return Convert.ToBoolean(combination[5]);
+            return Convert.ToBoolean(m_combination[5]);
         } else if ((upperTreeTiles.x == 0) && (upperTreeTiles.y == 0) && (upperTreeTiles.z == 1)) {
-            return Convert.ToBoolean(combination[6]);
+            return Convert.ToBoolean(m_combination[6]);
         } else if ((upperTreeTiles.x == 0) && (upperTreeTiles.y == 0) && (upperTreeTiles.z == 0)) {
-            return Convert.ToBoolean(combination[7]);
+            return Convert.ToBoolean(m_combination[7]);
         } else {
             throw new Exception("Something went terrebly wrong :(");
         }
